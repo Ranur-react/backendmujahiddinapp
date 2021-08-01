4020ce56575b281bea5bd92b1bb2cd9855a1dde8
Ranur-react <rahmatnur845@gmail.com>
2021-08-01 15:59:19 +0700

    Input data keluar lainnya

---
+++ dataUangKeluarLainnya/insert.php
@@ -0,0 +1,26 @@
+<?php
+include '../koneksi.php';
+if (isset($_POST['id_keluar'])) {
+
+    $id_dataspenerima = $_POST['id_keluar'];
+    $nama_penerima = $_POST['tanggal_keluar'];
+    $alamat_penerima = $_POST['uraian_keluar'];
+    $nohp_penerima = $_POST['jumlah_keluar'];
+
+    $query = mysqli_query($conn, " INSERT INTO `tb_uangkeluarlainnya` 
+    VALUES ('$id_dataspenerima', '$nama_penerima','$alamat_penerima','$nohp_penerima'); ");
+
+    if ($query) {
+        $data['pesan'] = "Data Anda Berhasil";
+        $data['status'] = true;
+    } else {
+        $data['pesan'] = "Data anda tidak valid";
+        $data['status'] = false;
+        $data['datatidakvakid'] = $_POST;
+    }
+} else {
+    $data['pesan'] = "Data Tidak Dapat Diakses";
+    $data['status'] = false;
+}
+echo json_encode($data);
+?>
