0f59112e6939ad5a256aec72234ac12ba66eafd9
Ranur-react <rahmatnur845@gmail.com>
2021-08-01 18:12:42 +0700

    Tampil data user

--- datauser/index.php
+++ datauser/index.php
@@ -1,5 +1,6 @@
 <?php
-include 'koneksi.php';
+include '../koneksi.php';
+
 $query = mysqli_query($conn, " SELECT * FROM tb_user;");
 
 if ($query) {
--- datauser/update.php
+++ datauser/update.php
@@ -1,5 +1,5 @@
 <?php
-include 'koneksi.php';
+include '../koneksi.php';
 if (isset($_POST['kode_user'])) {
 
     $kodeuser = $_POST['kode_user'];
