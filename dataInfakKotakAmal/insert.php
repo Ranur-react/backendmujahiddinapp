7a3a37da0bf8c7c3ff787156236a9c77bc833d2c
Ranur-react <rahmatnur845@gmail.com>
2021-08-01 12:22:01 +0700

    Insert data Infak Masuk

---
+++ dataInfakKotakAmal/insert.php
@@ -0,0 +1,25 @@
+<?php
+include '../koneksi.php';
+if (isset($_POST['id_datainfak'])) {
+
+    $kodkatgrinfak = $_POST['id_datainfak'];
+    $namakatgrinfak = $_POST['tanggal_datainfak'];
+    $namakatgrinfak = $_POST['idkatgr_datainfak'];
+    $namakatgrinfak = $_POST['jumlah_datainfak'];
+
+    $query = mysqli_query($conn, " INSERT INTO `tb_infak` (`kodkatgr_infak`, `namakatgr_infak`) 
+    VALUES ('$kodkatgrinfak', '$namakatgrinfak'); ");
+
+    if ($query) {
+        $data['pesan'] = "Data Anda Berhasil";
+        $data['status'] = true;
+    } else {
+        $data['pesan'] = "Data anda tidak valid";
+        $data['status'] = false;
+    }
+} else {
+    $data['pesan'] = "Data Tidak Dapat Diakses";
+    $data['status'] = false;
+}
+echo json_encode($data);
+?>
