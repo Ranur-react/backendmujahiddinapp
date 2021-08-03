32c1369c0abd6942745fe877332599728d03de98
Ranur-react <rahmatnur845@gmail.com>
2021-08-03 12:26:19 +0700

    Delete perbaiki

--- datauser/delete.php
+++ datauser/delete.php
@@ -2,7 +2,7 @@
 include 'koneksi.php';
 if (isset($_POST['kode_user'])) {
     $kodeuser = $_POST['kode_user'];
-    $query = mysqli_query($conn, "DELETE FROM tb_user WHERE kode_user = $kodeuser'");
+    $query = mysqli_query($conn, "DELETE FROM tb_user WHERE kode_user = '$kodeuser'");
     if (!$query) {
         $data['pesan'] = "Query atau data salah";
         $data['data'] = $_POST['kode_user'];
--- datauser/index.php
+++ datauser/index.php
@@ -1,6 +1,5 @@
 <?php
 include '../koneksi.php';
-
 $query = mysqli_query($conn, " SELECT * FROM tb_user;");
 
 if ($query) {
