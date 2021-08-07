7a88e677943936481847e9b4ea621883255d71bc
Ranur-react <rahmatnur845@gmail.com>
2021-08-07 22:43:46 +0700

    Tampilan User

--- datauser/index.php
+++ datauser/index.php
@@ -1,6 +1,6 @@
 <?php
 include '../koneksi.php';
-$query = mysqli_query($conn, " SELECT * FROM tb_user;");
+$query = mysqli_query($conn, "SELECT nama_user,nama_level,kode_user,username_user,password_user,level_user FROM tb_user join tb_level on id=level_user;");
 
 if ($query) {
     $database = [];
