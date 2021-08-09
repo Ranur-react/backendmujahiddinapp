402b35fba8d6c680d1ec126dd44f612ca4c3908c
Ranur-react <rahmatnur845@gmail.com>
2021-08-09 23:04:13 +0700

    Update data donasi

---
+++ datadonasi/update.php
@@ -0,0 +1,25 @@
+<?php
+include '../koneksi.php';
+if (isset($_POST['iddon_penerima'])) {
+
+    $a = $_POST['iddon_penerima'];
+    $b = $_POST['tanggal_penerima'];
+    $c = $_POST['id_penerima'];
+    $d = $_POST['id_jenispenerima'];
+    $e = $_POST['jumlah_donasi'];
+
+
+    $query = mysqli_query($conn, " UPDATE tb_penerimaan SET  tanggal_penerima= '$b', id_penerima= '$c', id_jenispenerima= '$d',   jumlah_donasi= '$e' WHERE iddon_penerima= '$a';");
+
+    if ($query) {
+        $data['pesan'] = "Data anda tidak valid";
+        $data['status'] = false;
+    } else {
+        $data['pesan'] = "Data Anda Berhail Update";
+        $data['status'] = true;
+    }
+} else {
+    $data['pesan'] = "Data Tidak Dapat Diakses";
+    $data['status'] = false;
+}
+echo json_encode($data);
