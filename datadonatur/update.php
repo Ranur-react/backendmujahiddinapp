c107769634a0996d897eac0838d0143160d03af0
Ranur-react <rahmatnur845@gmail.com>
2021-08-11 01:37:49 +0700

    update data donatur

---
+++ datadonatur/update.php
@@ -0,0 +1,25 @@
+<?php
+include '../koneksi.php';
+if (isset($_POST['kode_datadonatur'])) {
+
+    $a = $_POST['kode_datadonatur'];
+    $b = $_POST['nama_datadonatur'];
+    $c = $_POST['alamat_donatur'];
+    $d = $_POST['nohp_donatur'];
+
+
+    $query = mysqli_query($conn, "UPDATE tb_penerimaan SET  nama_datadonatur= '$b', alamat_donatur= '$c', nohp_donatur= '$d',   WHERE kode_datadonatur= '$a';");
+
+    if ($query) {
+        $data['pesan'] = "Data Anda Berhail Update";
+        $data['status'] = true;
+    } else {
+        $data['data'] =$_POST;
+        $data['pesan'] = "Data anda tidak valid";
+        $data['status'] = false;
+    }
+} else {
+    $data['pesan'] = "Data Tidak Dapat Diakses";
+    $data['status'] = false;
+}
+echo json_encode($data);
