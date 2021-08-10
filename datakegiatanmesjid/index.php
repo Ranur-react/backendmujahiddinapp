9c6ec8bfc3c1b86260b13d321383732798c58b3b
Ranur-react <rahmatnur845@gmail.com>
2021-08-10 23:30:38 +0700

    Tampil data kegiatan perbaiki

--- datakegiatanmesjid/index.php
+++ datakegiatanmesjid/index.php
@@ -1,6 +1,6 @@
 <?php
 include '../koneksi.php';
-$query = mysqli_query($conn, "SELECT no_datakegtn,nama_kegiatan,`hari`,`waktu_datakegiatan`, hari_datakegiatan, kode_datakegiatan,id_datapemateri FROM tb_datakegiatan join `tb_kegiatan` on kode_kegiatan=kode_datakegiatan join tb_hari on hari_datakegiatan=id;");
+$query = mysqli_query($conn, "SELECT no_datakegtn,concat(`hari`,'-',waktu_datakegiatan) as infowaktu,nama_kegiatan,`waktu_datakegiatan`, hari_datakegiatan, kode_datakegiatan,id_datapemateri FROM tb_datakegiatan join `tb_kegiatan` on kode_kegiatan=kode_datakegiatan join tb_hari on hari_datakegiatan=id;");
 
 if ($query) {
     $database = [];
