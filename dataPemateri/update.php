8c0668041e92c0d7031c2cab9d1de4a565d1bae1
Ranur-react <rahmatnur845@gmail.com>
2021-08-10 17:42:47 +0700

    Upload data

---
+++ dataPemateri/update.php
@@ -0,0 +1,22 @@
+<?php
+include '../koneksi.php';
+if (isset($_POST['kode_pemateri'])) {
+    $kode_pemateri = $_POST['kode_pemateri'];
+    $nama_pemateri = $_POST['nama_pemateri'];
+
+   
+    $query = mysqli_query($conn, "UPDATE tb_penerimaan SET  nama_pemateri='$nama_pemateri' WHERE kode_pemateri= '$kode_pemateri';");
+    if ($query) {
+        $data['pesan'] = "Data Anda Berhasil Update";
+        $data['status'] = true;
+    } else {
+        $data['pesan'] = "Data anda tidak valid";
+        $data['status'] = false;
+        $data['data'] = $_POST;
+    }
+} else {
+    $data['pesan'] = "Data Tidak Dapat Diakses";
+    $data['status'] = false;
+}
+echo json_encode($data);
+?>
