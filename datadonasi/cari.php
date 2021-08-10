e2a3738552110d06673bba5845f089d9413cf161
Ranur-react <rahmatnur845@gmail.com>
2021-08-10 12:41:54 +0700

    pecarian

---
+++ datadonasi/cari.php
@@ -0,0 +1,23 @@
+<?php
+include '../koneksi.php';
+$key1 = '';
+$key1 = $_POST['key'];
+
+$query = mysqli_query($conn, "select iddon_penerima,concat(nama_penerima,' ( ',nama_jenisdon,')',' ~ ',alamat_penerima) as perner,jumlah_donasi,id_penerima,id_jenispenerima,tanggal_penerima from tb_penerimaan join tb_datapenerima on id_dataspenerima=id_penerima join tb_jenisdonasi on id_jenispenerima=id_jenisdon where jumlah_donasi LIKE '%$key1%' or nama_penerima LIKE '%$key1%' or nama_jenisdon like '%$key1%' or alamat_penerima like '%$key1%' or iddon_penerima like '%$key1%';");
+
+if ($query) {
+    $database = [];
+    while ($d = mysqli_fetch_array($query)) {
+        $database[] = $d;
+    }
+    $data['data'] = $database;
+
+    $data['pesan'] = "";
+    $data['status'] = true;
+}else{
+    $data['data'] = "";
+    $data['pesan'] = "Data gagal diambil dari database";
+    $data['status'] = false;
+}
+echo json_encode($data);
+?>
