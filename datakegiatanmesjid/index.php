8ef6cdb959c8daeb61fc82f85748ee81853429e7
Ranur-react <rahmatnur845@gmail.com>
2021-08-10 23:27:35 +0700

    upload data perubana tampil data kegiatahnm masjdi

--- datakegiatanmesjid/index.php
+++ datakegiatanmesjid/index.php
@@ -1,6 +1,6 @@
 <?php
 include '../koneksi.php';
-$query = mysqli_query($conn, "SELECT nama_kegiatan,`hari`,`waktu_datakegiatan` FROM tb_datakegiatan join `tb_kegiatan` on kode_kegiatan=kode_datakegiatan join tb_hari on hari_datakegiatan=id;");
+$query = mysqli_query($conn, "SELECT no_datakegtn,nama_kegiatan,`hari`,`waktu_datakegiatan`, hari_datakegiatan, kode_datakegiatan,id_datapemateri FROM tb_datakegiatan join `tb_kegiatan` on kode_kegiatan=kode_datakegiatan join tb_hari on hari_datakegiatan=id;");
 
 if ($query) {
     $database = [];
@@ -16,6 +16,7 @@ if ($query) {
     $data['pesan'] = "Data gagal diambil dari database";
     $data['status'] = false;
 }
+
 echo json_encode($data);
 
 ?>
