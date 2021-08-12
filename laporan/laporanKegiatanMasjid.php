8786a19cbbc03ffd81f2f46d7d74a689749f8f8c
Ranur-react <rahmatnur845@gmail.com>
2021-08-12 16:55:03 +0700

    Laporan Kegiatan

--- laporan/laporanKegiatanMasjid.php
+++ laporan/laporanKegiatanMasjid.php
@@ -1,20 +1,52 @@
 <?php
 include '../koneksi.php';
 $query = mysqli_query($conn, " SELECT * FROM tb_kegiatan;");
-foreach ($d = mysqli_fetch_array($query) as $d ) :
+foreach ($d = mysqli_fetch_array($query) as $d) :
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laporan Kegiatan</title>
-</head>
-<body>
-    x
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Laporan Kegiatan</title>
+    </head>
+
+    <body>
+        <table>
+            <tr>
+                <td rowspan="2">X</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </body>
+
+    </html>
 <?php
 endforeach;
 ?>
