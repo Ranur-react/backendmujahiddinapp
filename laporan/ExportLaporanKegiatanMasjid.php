f938fbc4cb851742f33e5109183df9555f557de2
Ranur-react <rahmatnur845@gmail.com>
2021-08-12 22:44:32 +0700

    library pdf

--- laporan/ExportLaporanKegiatanMasjid.php
+++ laporan/ExportLaporanKegiatanMasjid.php
@@ -4,7 +4,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 $mpdf = new \Mpdf\Mpdf();
 
-$html='<thead><tr>    <th style="text-align: center;" rowspan="2">Kode kegiatan</th><th style="text-align: center;"rowspan="2">Nama kegiatan</th>    <th colspan="7" style="text-align: center;">Hari</th>    <th style="text-align: center;" rowspan="2">Waktu</th><th style="text-align: center;" rowspan="2">Nama Pemateri</th></tr><tr><th style="text-align: center;" >Senin</th><th style="text-align: center;">Selasa</th><th style="text-align: center;">Rabu</th><th style="text-align: center;">Kamis</th><th style="text-align: center;">Jumat</th><th style="text-align: center;">Sabtu</th><th style="text-align: center;">Minggu</th></tr></thead><tbody>';
+$html='<thead style="font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;"><tr>    <th style="text-align: center;" rowspan="2">Kode kegiatan</th><th style="text-align: center;"rowspan="2">Nama kegiatan</th>    <th colspan="7" style="text-align: center;">Hari</th>    <th style="text-align: center;" rowspan="2">Waktu</th><th style="text-align: center;" rowspan="2">Nama Pemateri</th></tr><tr><th style="text-align: center;" >Senin</th><th style="text-align: center;">Selasa</th><th style="text-align: center;">Rabu</th><th style="text-align: center;">Kamis</th><th style="text-align: center;">Jumat</th><th style="text-align: center;">Sabtu</th><th style="text-align: center;">Minggu</th></tr></thead><tbody>';
 
 $mpdf->WriteHTML($html);
 $mpdf->Output();
