933554c3f2f4b95536188823694c49090b3d46ec
Ranur-react <rahmatnur845@gmail.com>
2021-08-12 23:15:03 +0700

    library pdf

--- laporan/ExportLaporanKegiatanMasjid.php
+++ laporan/ExportLaporanKegiatanMasjid.php
@@ -27,21 +27,9 @@ $html='
         <tbody>
         </tbody>
 ';
-include '../koneksi.php';
-            $query = mysqli_query($conn, "SELECT no_datakegtn,concat(`hari`,'-',waktu_datakegiatan) as infowaktu,nama_kegiatan,`waktu_datakegiatan`, hari_datakegiatan, kode_datakegiatan,id_datapemateri,nama_pemateri FROM tb_datakegiatan join `tb_kegiatan` on kode_kegiatan=kode_datakegiatan join tb_hari on hari_datakegiatan=id join tb_pemateri on kode_pemateri=id_datapemateri;");
-            function hariceklis($d,$var)
-                {
-                    if($d == $var){
-                        return 'V';
-                    }
-                    
-                }
-            // while ($data = mysqli_fetch_array($query)) {
-                 
-         
-            // }
+
 $html+='
-        <tbody>
+
         </table>
     </body>
     
