8467a162412dd3f64376f1913e0d167dd1a8ed80
Ranur-react <rahmatnur845@gmail.com>
2021-08-12 18:41:19 +0700

    Laporan Kegiatan

--- laporan/laporanKegiatanMasjid.php
+++ laporan/laporanKegiatanMasjid.php
@@ -1,41 +1,55 @@
-<?php
-include '../koneksi.php';
-$query = mysqli_query($conn, " SELECT * FROM tb_kegiatan;");
-foreach ($d = mysqli_fetch_array($query) as $d) :
-?>
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Laporan Kegiatan</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laporan Kegiatan</title>
+</head>
 
-    <body>
-        <table border="1">
+<body>
+    <table align="center" border="1">
+        <thead>
             <tr>
-                <td rowspan="2">Kode kegiatan</td>
-                <td rowspan="2">Nama kegiatan</td>
-                <td colspan="7">Hari</td>
-        
-                <td rowspan="2">Waktu</td>
-                <td rowspan="2">Nama Pemateri</td>
+                <th rowspan="2">Kode kegiatan</th>
+                <th rowspan="2">Nama kegiatan</th>
+                <th colspan="7">Hari</th>
+
+                <th rowspan="2">Waktu</th>
+                <th rowspan="2">Nama Pemateri</th>
             </tr>
             <tr>
-            <td>Senin</td>
-                <td>Selasa</td>
-                <td>Rabu</td>
-                <td>Kamis</td>
-                <td>Jum'at</td>
-                <td>Sabtu</td>
-                <td>Minggu</td>
+                <th>Senin</th>
+                <th>Selasa</th>
+                <th>Rabu</th>
+                <th>Kamis</th>
+                <th>Jum'at</th>
+                <th>Sabtu</th>
+                <th>Minggu</th>
             </tr>
-        </table>
-    </body>
+        </thead>
+        <tbody>
+            <?php
+            include '../koneksi.php';
+            $query = mysqli_query($conn, "SELECT no_datakegtn,concat(`hari`,'-',waktu_datakegiatan) as infowaktu,nama_kegiatan,`waktu_datakegiatan`, hari_datakegiatan, kode_datakegiatan,id_datapemateri,nama_pemateri FROM tb_datakegiatan join `tb_kegiatan` on kode_kegiatan=kode_datakegiatan join tb_hari on hari_datakegiatan=id join tb_pemateri on kode_pemateri=id_datapemateri;");
+            foreach ($d = mysqli_fetch_array($query) as $data) :
+            ?>
+                <td><?php echo $data['no_datakegtn']; ?></td>
+                <td><?php echo $data['nama_kegiatan']; ?></td>
+                <td><?= $data['hari_datakegiatan']; ?></td>
+                <td><?php echo $data['no_datakegtn']; ?></td>
+                <td><?php echo $data['no_datakegtn']; ?></td>
+                <td><?php echo $data['no_datakegtn']; ?></td>
+                <td><?php echo $data['no_datakegtn']; ?></td>
+                </tr>
+            <?php
+            endforeach;
+            ?>
+        </tbody>
 
-    </html>
-<?php
-endforeach;
-?>
+
+    </table>
+</body>
+
+</html>
