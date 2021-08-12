59a82d1851e33dd3f28d474d7d0684ee8a0d99bf
Ranur-react <rahmatnur845@gmail.com>
2021-08-12 22:47:12 +0700

    library pdf

--- laporan/ExportLaporanKegiatanMasjid.php
+++ laporan/ExportLaporanKegiatanMasjid.php
@@ -1,10 +1,35 @@
-
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
 $mpdf = new \Mpdf\Mpdf();
+$html='
+<title>Laporan Kegiatan</title>
+ 
+</head>
 
-$html='<thead style="font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;border: 1px solid #ddd;padding: 8px;"><tr>    <th style="text-align: center;" rowspan="2">Kode kegiatan</th><th style="text-align: center;"rowspan="2">Nama kegiatan</th>    <th colspan="7" style="text-align: center;">Hari</th>    <th style="text-align: center;" rowspan="2">Waktu</th><th style="text-align: center;" rowspan="2">Nama Pemateri</th></tr><tr><th style="text-align: center;" >Senin</th><th style="text-align: center;">Selasa</th><th style="text-align: center;">Rabu</th><th style="text-align: center;">Kamis</th><th style="text-align: center;">Jumat</th><th style="text-align: center;">Sabtu</th><th style="text-align: center;">Minggu</th></tr></thead><tbody>';
+<body>
+    <table id="customers" align="center" border="1">
+        <thead>
+            <tr>
+                <th style="text-align: center;" rowspan="2">Kode kegiatan</th>
+                <th style="text-align: center;" rowspan="2">Nama kegiatan</th>
+                <th colspan="7" style="text-align: center;">Hari</th>
+
+                <th style="text-align: center;" rowspan="2">Waktu</th>
+                <th style="text-align: center;" rowspan="2">Nama Pemateri</th>
+            </tr>
+            <tr>
+                <th style="text-align: center;" >Senin</th>
+                <th style="text-align: center;">Selasa</th>
+                <th style="text-align: center;">Rabu</th>
+                <th style="text-align: center;">Kamis</th>
+                <th style="text-align: center;">Jumat</th>
+                <th style="text-align: center;">Sabtu</th>
+                <th style="text-align: center;">Minggu</th>
+            </tr>
+        </thead>
+        <tbody>
+';
 
 $mpdf->WriteHTML($html);
 $mpdf->Output();
