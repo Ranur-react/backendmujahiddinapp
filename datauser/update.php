8c56814f8b1db6ef32266b2a269d13ff9c596ec9
Ranur-react <rahmatnur845@gmail.com>
2021-08-12 15:46:15 +0700

    Update data user

--- datauser/update.php
+++ datauser/update.php
@@ -8,7 +8,7 @@ if (isset($_POST['kode_user'])) {
     $passworduser = $_POST['password_user'];
     $leveluser = $_POST['level_user'];
 
-    $query = mysqli_query($conn, " UPDATE tb_mujahiddin . db_user SET  nama_user= '$namauser', username_user= '$usernameuser', password_user= '$passworduser',   level_user= '$leveluser' WHERE kode_user= '$kodeuser';");
+    $query = mysqli_query($conn, " UPDATE tb_mujahiddin SET  nama_user= '$namauser', username_user= '$usernameuser', password_user= '$passworduser',   level_user= '$leveluser' WHERE kode_user= '$kodeuser';");
 
     if ($query) {
         $data['pesan'] = "Data anda tidak valid";
