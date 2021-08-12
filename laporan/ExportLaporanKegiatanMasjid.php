e3974657d5a4c2abbbe67a735a1a9281da7b2956
Ranur-react <rahmatnur845@gmail.com>
2021-08-12 22:48:49 +0700

    library pdf

--- laporan/ExportLaporanKegiatanMasjid.php
+++ laporan/ExportLaporanKegiatanMasjid.php
@@ -3,7 +3,13 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 $mpdf = new \Mpdf\Mpdf();
 $html='
-<title>Laporan Kegiatan</title>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
  
 </head>
 
