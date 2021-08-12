375415b331ce0f331cb2330deecef5eb013baf4f
Ranur-react <rahmatnur845@gmail.com>
2021-08-12 22:50:57 +0700

    library pdf

--- laporan/ExportLaporanKegiatanMasjid.php
+++ laporan/ExportLaporanKegiatanMasjid.php
@@ -4,8 +4,34 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $mpdf = new \Mpdf\Mpdf();
 $html='
 
-adas
-asdasd
+<table id="customers" align="center" border="1">
+        <thead>
+            <tr>
+                <th style="text-align: center;" rowspan="2">Kode kegiatan</th>
+                <th style="text-align: center;" rowspan="2">Nama kegiatan</th>
+                <th colspan="7" style="text-align: center;">Hari</th>
+
+                <th style="text-align: center;" rowspan="2">Waktu</th>
+                <th style="text-align: center;" rowspan="2">Nama Pemateri</th>
+            </tr>
+            <tr>
+                <th style="text-align: center;" >Senin</th>
+                <th style="text-align: center;">Selasa</th>
+                <th style="text-align: center;">Rabu</th>
+                <th style="text-align: center;">Kamis</th>
+                <th style="text-align: center;">Jumat</th>
+                <th style="text-align: center;">Sabtu</th>
+                <th style="text-align: center;">Minggu</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+
+
+        </table>
+    </body>
+    
+    </html>
 
 ';
 
