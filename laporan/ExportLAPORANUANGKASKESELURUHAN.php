e661ae28967622bf5605add9461e24be0149f8e4
Ranur-react <rahmatnur845@gmail.com>
2021-08-13 11:38:26 +0700

    Lap data keseluruahn

--- laporan/ExportLAPORANUANGKASKESELURUHAN.php
+++ laporan/ExportLAPORANUANGKASKESELURUHAN.php
@@ -17,8 +17,8 @@ $html='
         <tbody>';
         include '../koneksi.php';
         $queryDonasi = mysqli_query($conn, " select*from tb_donasimasuk join tb_datadonatur on iddon_donmasuk=kode_datadonatur;");
-        $queryPenermaan = mysqli_query($conn, " SELECT * FROM tb_penerimaan;");
-        $queryInfak = mysqli_query($conn, " SELECT * FROM tb_datainfak;");
+        // $queryPenermaan = mysqli_query($conn, " SELECT * FROM tb_penerimaan;");
+        $queryInfak = mysqli_query($conn, " SELECT * FROM tb_datainfak join `tb_infak` on kodkatgr_infak=kodkatgr_infak;");
         $queryKeluar = mysqli_query($conn, " SELECT * FROM tb_uangkeluarlainnya;");
 
 
@@ -38,6 +38,18 @@ $html='
                 <td style="text-align: left;"> Donasi Masuk ('.$data["ket_donmasuk"].')</td>
                 <td style="text-align: center;">'.$data["tanggal_donmasuk"].'</td>
                 <td style="text-align: left;">'.rupiah($data["jumlah_donmasuk"]).'</td>
+            </tr>
+                ';
+
+        }
+        while ($data = mysqli_fetch_array($queryInfak)) {
+            $n+=1;
+            $html.='
+                <tr>
+                <td style="text-align: center;">'.$n.'</td>
+                <td style="text-align: left;"> Infak Masuk ('.$data["namakatgr_infak"].')</td>
+                <td style="text-align: center;">'.$data["tanggal_datainfak"].'</td>
+                <td style="text-align: left;">'.rupiah($data["jumlah_datainfak"]).'</td>
             </tr>
                 ';
 
