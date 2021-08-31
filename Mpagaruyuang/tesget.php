e1e8f77ba3e0885e4b4a5b66565bf7987178c93e
Ranur-react <rahmatnur845@gmail.com>
2021-08-31 18:55:20 +0700

    tes get status

--- Mpagaruyuang/tesget.php
+++ Mpagaruyuang/tesget.php
@@ -6,4 +6,5 @@ curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($curl);
 curl_close($curl);
-echo $response['data'];
+$data=json_decode($response);
+echo $data['data'];
