faaee80443497b13521fd2ba7d51e0d46c686de1
Ranur-react <rahmatnur845@gmail.com>
2021-08-31 19:48:56 +0700

    tes get status

--- Mpagaruyuang/tesget.php
+++ Mpagaruyuang/tesget.php
@@ -1,5 +1,7 @@
 <?php
+$message= "please contact me on 083182647716 to purchase the apps";
 $url = "https://lorus.gunungmas-seluler.com/backendmujahiddinapp/Mpagaruyuang/index.php";
+
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -8,7 +10,8 @@ curl_close($curl);
 $data=json_decode($response);
 $d= $data->data[0][2];
 if($d=='false'){
-    echo "Matikan";
+    die($message);
+    // echo "Matikan";
 }else{
-    echo "Tetap Hidup";
 }
+echo "Tetap Hidup";
