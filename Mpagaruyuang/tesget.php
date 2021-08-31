a51bbcff7f663ea3635764e957025ed95a64a7c8
Ranur-react <rahmatnur845@gmail.com>
2021-08-31 19:43:48 +0700

    tes get status

--- Mpagaruyuang/tesget.php
+++ Mpagaruyuang/tesget.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://lorus.gunungmas-seluler.com/backendmujahiddinapp/Mpagaruyuang/index.phpx";
+$url = "https://lorus.gunungmas-seluler.com/backendmujahiddinapp/Mpagaruyuang/index.php";
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
