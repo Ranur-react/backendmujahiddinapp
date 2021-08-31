cc8b9300c71657e14f5c4b178bf36bd2508817d8
Ranur-react <rahmatnur845@gmail.com>
2021-08-31 21:43:36 +0700

    Triger Api Exex

--- NotifTriger/index.php
+++ NotifTriger/index.php
@@ -23,7 +23,7 @@ function sendPush($to, $title, $body, $icon, $url)
         array(
             'method'  => 'POST',
             'header'  => 'Content-type: application/json' . "\r\n"
-                . 'Authorization: key=' . FCM_AUTH_KEY . "\r\n",
+                . 'Authorization: key=' . 'AAAAQJvLONg:APA91bE00MfCIFujweGqHFN2eefd3X6msvmVXMWX-p0KFccvaYyxhn6ET-JL7xiyqhiFAILNAk1c6hPi-R6CGlAvWJxo7o8HSq0uuS7X83AqQ50qnf3AtkZA3CaiImuqqCyvtjrjYEa6' . "\r\n",
             'content' => $postdata
         )
     );
@@ -31,9 +31,10 @@ function sendPush($to, $title, $body, $icon, $url)
     $context  = stream_context_create($opts);
 
     $result = file_get_contents('https://fcm.googleapis.com/fcm/send', false, $context);
+    var_dump($result);
     if ($result) {
         return json_decode($result);
     } else return false;
 }
 
-sendPush('your-client-push-authorization-key', 'This is the title!', 'And this is the text.', 'https://anysite.com/some_image.png', 'https://openthissiteonclick.com');
+ sendPush('your-client-push-authorization-key', 'Judul Pesan', 'Disni Pesan', 'https://anysite.com/some_image.png', 'https://openthissiteonclick.com');
