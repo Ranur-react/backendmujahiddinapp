74eb361f8d8ff45d1f006f5fa03e1821bbad8a2d
Ranur-react <rahmatnur845@gmail.com>
2021-09-01 17:22:52 +0700

    Notif Pemateri 10 devices last

--- Notif_Pemateri/index.php
+++ Notif_Pemateri/index.php
@@ -5,13 +5,29 @@ include '../Notiflibrary/index.php';
 
 
 //key dan configurations
-$fetchQry1 = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM tb_FCMConfig where tb_FCMConfig.key='FCM_AUTH_KEY';"));
+    $fetchQry1 = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM tb_FCMConfig where tb_FCMConfig.key='FCM_AUTH_KEY';"));
 $keyApi=$fetchQry1['value'];
-$fetchQry2 = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM tb_FCMConfig where tb_FCMConfig.key='FCM_URL';"));
-echo $urlApi=$fetchQry2['value'];
-
-
+    $fetchQry2 = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM tb_FCMConfig where tb_FCMConfig.key='FCM_URL';"));
+$urlApi=$fetchQry2['value'];
 //---end
+
+//Kontent Config
+$title="Pemateri ". $NamaUstadz." Sudah Datang";
+$body="Hallo Jema'ah Mesjid Mujahidin, Pemateri Kegiatan deng Ustadz ". $NamaUstadz ." Akan segera berlangsung, karena ustadz sudah di konfirmasi datang";
+    $fetchQry3 = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM tb_FCMConfig where tb_FCMConfig.key='utadzsomad';"));
+$imagesContent= $fetchQry3['value'];
+$fetchQry4 = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM tb_FCMConfig where tb_FCMConfig.key='FCM_ICON';"));
+$icon= $fetchQry4['value'];
+    $fetchQry5 = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM tb_FCMConfig where tb_FCMConfig.key='FCM_DEFAULT_URI_APP_ANDROID';"));
+$urldst= $fetchQry5['value'];
+//end
+
+
+//Destionatyions Device
+while ($d = mysqli_fetch_array(mysqli_query($conn, "Select * FROM tb_FCM_Devices where last_online in (SELECT MAX(last_online) from tb_FCM_Devices Group by date(last_online) ) order by last_online desc limit 10;"))) {
+    echo "<br> - - -";
+    echo $d['token_id'];
+}
 // sendPush($to, $title, $body, $imagesContent, $icon, $urldst, $keyApi, $urlApi)
 
 
